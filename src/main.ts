f60cddfcd70b708fdf0c43a7bd8416609aa965df
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-05-31 01:11:43 +0900

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+// Require the necessary discord.js classes
+import { GatewayIntentBits, Client, Partials, Message, Events } from 'discord.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Create a new client instance
+const client = new Client({
+    intents: [
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+    ],
+    partials: [Partials.Message, Partials.Channel],
+});
+
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.BOT_TOKEN);
