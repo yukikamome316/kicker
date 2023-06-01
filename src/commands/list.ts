8062fc122e8f42b95c5bfdaa7d596f9319c6f88a
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-02 04:53:24 +0900

    feat: added embeded message in /list command

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,38 +1,44 @@
-import { RPCCloseEventCodes } from "discord.js";
-import { RequestManager, Role, GuildMember } from "discord.js";
-import { Interaction, CommandInteraction, SlashCommandBuilder, SlashCommandRoleOption } from "discord.js";
-import { RawRoleData, RawUserData } from "discord.js/typings/rawDataTypes";
+import { Role, Interaction, SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import { string } from "zod";
 
 export const data = new SlashCommandBuilder()
   .setName('list')
-  .setDescription('make a list members with/without specific roles')
+  .setDescription('make a list members positive/negative specific roles')
   .addStringOption(option =>
-		option.setName('with')
+		option.setName('positive')
 			.setDescription('Roles that members have')
       .setRequired(false))
   .addStringOption(option => 
-    option.setName('without')
+    option.setName('negative')
       .setDescription('Roles that members don\'t have')
       .setRequired(false))
+  .addIntegerOption(option =>
+    option.setName('head_count')
+      .setDescription('Showing the names of the number of people')
+      .setRequired(false)
+      .setMinValue(1));
 
 export const execute = async (interaction: Interaction) => {
   if (!interaction.isChatInputCommand()) return;
-  // const plus_1 = interaction.options.get('plus_1');
-  // const minus_1 = interaction.options.get('minus_1');
 
-  const rolesWithMemberRawText = interaction.options.getString('with') ?? 'None';
-  const rolesWithoutMemberRawText = interaction.options.getString('without') ?? 'None';
-  
+  const DEFAULT_HEAD_COUNT = 10;
+
+  const rolesWithMemberRawText = interaction.options.getString('positive') ?? 'None';
+  const rolesWithoutMemberRawText = interaction.options.getString('negative') ?? 'None';
+  const headCount = interaction.options.getInteger('head_count') ?? DEFAULT_HEAD_COUNT;
+
   const roleRegex = /(<@&\w+>)/g;
   const rolesWithMemberText = Array.from(
-    rolesWithMemberRawText.matchAll(roleRegex), match => match[1]);
+    rolesWithMemberRawText.matchAll(roleRegex) ?? 'None', match => match[1]);
   const rolesWithoutMemberText = Array.from(
-    rolesWithoutMemberRawText.matchAll(roleRegex), match => match[1]);
+    rolesWithoutMemberRawText.matchAll(roleRegex) ?? 'None', match => match[1]);
   
   let rolesWithMember: Role[] = [];
   let rolesWithoutMember: Role[] = [];
 
   const getRoleByMention = (mention: string): Role | null => {
+    if (!mention) return null;
+
     const match = mention.match(/^<@&(\d+)>$/);
     if (match) {
       const roleId = match[1];
@@ -55,4 +61,41 @@ export const execute = async (interaction: Interaction) => {
       rolesWithoutMember.push(role);
   });
 
+  const allMembers = interaction.guild?.members.cache;
+  if (!allMembers) return;
+
+  const members = allMembers.filter(member => {
+    const hasAllRoles = rolesWithMember.every(role => member.roles.cache.has(role.id));
+    const hasNoRoles = rolesWithoutMember.every(role => !member.roles.cache.has(role.id));
+  
+    return hasAllRoles && hasNoRoles;
+  });
+
+  const rolesWithMemberString = rolesWithMember.map(role => '<@&' + role.id + '>').join(' ');
+  const rolesWithoutMemberString = rolesWithoutMember.map(role => '<@&' + role.id + '>').join(' ');
+  
+  const membersCount = members ? members.size : 0;
+  
+  const getHeadMembers = (n: number) => {
+    let tmp = [];
+    for (let i = 0; i < n; i++) {
+      if (i >= members.size) break;
+      tmp.push(members.at(i)?.displayName);
+    }
+    return tmp.join(', ');
+  };
+  
+  const headName = `Head (count=${headCount})`;
+  const embedToSend = new EmbedBuilder()
+  .setColor("Blue")
+  .setTitle('Search result')
+  .addFields(
+    { name: 'User count', value: membersCount.toString()},
+    { name: headName, value: getHeadMembers(headCount)},
+    { name: 'Positive', value: rolesWithMemberString || 'None', inline: true},
+    { name: 'Negative', value: rolesWithoutMemberString || 'None', inline: true},
+    )
+    .setTimestamp();
+
+  interaction.reply({ embeds: [embedToSend]});
 };
