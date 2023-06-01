542dcc977b128f1584c9fe33a3ce12ae96a237ec
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-02 03:27:19 +0900

    feat: make a list of roles in command's options

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -17,5 +17,42 @@ export const data = new SlashCommandBuilder()
 
 export const execute = async (interaction: Interaction) => {
   if (!interaction.isChatInputCommand()) return;
+  // const plus_1 = interaction.options.get('plus_1');
+  // const minus_1 = interaction.options.get('minus_1');
+
+  const rolesWithMemberRawText = interaction.options.getString('with') ?? 'None';
+  const rolesWithoutMemberRawText = interaction.options.getString('without') ?? 'None';
+  
+  const roleRegex = /(<@&\w+>)/g;
+  const rolesWithMemberText = Array.from(
+    rolesWithMemberRawText.matchAll(roleRegex), match => match[1]);
+  const rolesWithoutMemberText = Array.from(
+    rolesWithoutMemberRawText.matchAll(roleRegex), match => match[1]);
+  
+  let rolesWithMember: Role[] = [];
+  let rolesWithoutMember: Role[] = [];
+
+  const getRoleByMention = (mention: string): Role | null => {
+    const match = mention.match(/^<@&(\d+)>$/);
+    if (match) {
+      const roleId = match[1];
+      const role = interaction.guild?.roles.cache.get(roleId);
+      if (role instanceof Role) {
+        return role;
+      }
+    }
+    return null;
+  };
+
+  rolesWithMemberText.forEach(text => {
+    const role = getRoleByMention(text);
+    if (role)
+      rolesWithMember.push(role);
+  });
+  rolesWithoutMemberText.forEach(text => {
+    const role = getRoleByMention(text);
+    if (role)
+      rolesWithoutMember.push(role);
+  });
 
 };
