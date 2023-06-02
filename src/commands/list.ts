90ca6c0e4511744fca6c070b93326765f1f3536a
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-06-02 14:14:54 +0900

    fix: call deferReply for timeout

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,4 +1,8 @@
-import { Role, Interaction, SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import { Events, Role, Interaction, SlashCommandBuilder, EmbedBuilder, Collection, GuildMember, GuildMemberManager, GuildMemberRoleManager } from "discord.js";
+import { client } from '../main';
+import { CommandInteraction } from "discord.js";
+
+const DEFAULT_HEAD_COUNT = 10;
 
 export const data = new SlashCommandBuilder()
   .setName('list')
@@ -17,10 +21,91 @@ export const data = new SlashCommandBuilder()
       .setRequired(false)
       .setMinValue(1));
 
+interface RoleMemberList {
+  count: Number,
+  head: string,
+  positive: string,
+  negative: string,
+}
+
+const getRoleMemberList = async (
+  interaction: CommandInteraction,
+  headCount: number,
+  rolesWithMemberText: string[],
+  rolesWithoutMemberText: string[]): Promise<RoleMemberList | null> => {
+    let rolesWithMember: Role[] = [];
+    let rolesWithoutMember: Role[] = [];
+
+    const getRoleByMention = (mention: string): Role | null => {
+      if (!mention) return null;
+
+      const match = mention.match(/^<@&(\d+)>$/);
+      if (match) {
+        const roleId = match[1];
+        const role = interaction.guild?.roles.cache.get(roleId);
+        if (role instanceof Role) {
+          return role;
+        }
+      }
+      return null;
+    };
+
+    rolesWithMemberText.forEach(text => {
+      const role = getRoleByMention(text);
+      if (role)
+        rolesWithMember.push(role);
+    });
+    rolesWithoutMemberText.forEach(text => {
+      const role = getRoleByMention(text);
+      if (role)
+        rolesWithoutMember.push(role);
+    });
+
+    const guild = await client.guilds.fetch(interaction.guild?.id ?? "");
+    const allMembers: GuildMember[] = [];
+
+    try {
+      await guild.members.fetch();
+
+      guild.members.cache.forEach((member) => {
+        allMembers.push(member);
+      });
+    } catch (error) {
+      console.error("Failed to fetch guild members:", error);
+      return null;
+    }
+
+    const members = allMembers.filter(member => {
+      const hasAllRoles = rolesWithMember.every(role => member.roles.cache.has(role.id));
+      const hasNoRoles = rolesWithoutMember.every(role => !member.roles.cache.has(role.id));
+    
+      return hasAllRoles && hasNoRoles;
+    });
+
+    const rolesWithMemberString = rolesWithMember.map(role => '<@&' + role.id + '>').join(' ');
+    const rolesWithoutMemberString = rolesWithoutMember.map(role => '<@&' + role.id + '>').join(' ');
+    
+    const membersCount = members ? members.length : 0;
+    
+    const getHeadMembers = (n: number) => {
+      let tmp = [];
+      for (let i = 0; i < n; i++) {
+        if (i >= members.length) break;
+        tmp.push(members.at(i)?.displayName);
+      }
+      return tmp.join(', ');
+    };
+
+    return {
+      count: membersCount,
+      head: getHeadMembers(headCount) || 'None',
+      positive: rolesWithMemberString || 'None',
+      negative: rolesWithoutMemberString || 'None',
+    };
+  };
+
 export const execute = async (interaction: Interaction) => {
   if (!interaction.isChatInputCommand()) return;
-
-  const DEFAULT_HEAD_COUNT = 10;
 
   const rolesWithMemberRawText = interaction.options.getString('positive') ?? 'None';
   const rolesWithoutMemberRawText = interaction.options.getString('negative') ?? 'None';
@@ -31,70 +116,28 @@ export const execute = async (interaction: Interaction) => {
     rolesWithMemberRawText.matchAll(roleRegex) ?? 'None', match => match[1]);
   const rolesWithoutMemberText = Array.from(
     rolesWithoutMemberRawText.matchAll(roleRegex) ?? 'None', match => match[1]);
+
+  await interaction.deferReply();
   
-  let rolesWithMember: Role[] = [];
-  let rolesWithoutMember: Role[] = [];
+  const list = await getRoleMemberList(
+    interaction,
+    headCount,
+    rolesWithMemberText,
+    rolesWithoutMemberText);
 
-  const getRoleByMention = (mention: string): Role | null => {
-    if (!mention) return null;
+  if (!list) return;
 
-    const match = mention.match(/^<@&(\d+)>$/);
-    if (match) {
-      const roleId = match[1];
-      const role = interaction.guild?.roles.cache.get(roleId);
-      if (role instanceof Role) {
-        return role;
-      }
-    }
-    return null;
-  };
-
-  rolesWithMemberText.forEach(text => {
-    const role = getRoleByMention(text);
-    if (role)
-      rolesWithMember.push(role);
-  });
-  rolesWithoutMemberText.forEach(text => {
-    const role = getRoleByMention(text);
-    if (role)
-      rolesWithoutMember.push(role);
-  });
-
-  const allMembers = interaction.guild?.members.cache;
-  if (!allMembers) return;
-
-  const members = allMembers.filter(member => {
-    const hasAllRoles = rolesWithMember.every(role => member.roles.cache.has(role.id));
-    const hasNoRoles = rolesWithoutMember.every(role => !member.roles.cache.has(role.id));
-  
-    return hasAllRoles && hasNoRoles;
-  });
-
-  const rolesWithMemberString = rolesWithMember.map(role => '<@&' + role.id + '>').join(' ');
-  const rolesWithoutMemberString = rolesWithoutMember.map(role => '<@&' + role.id + '>').join(' ');
-  
-  const membersCount = members ? members.size : 0;
-  
-  const getHeadMembers = (n: number) => {
-    let tmp = [];
-    for (let i = 0; i < n; i++) {
-      if (i >= members.size) break;
-      tmp.push(members.at(i)?.displayName);
-    }
-    return tmp.join(', ');
-  };
-  
   const headName = `Head (count=${headCount})`;
   const embedToSend = new EmbedBuilder()
   .setColor("Blue")
   .setTitle('Search result')
   .addFields(
-    { name: 'User count', value: membersCount.toString()},
-    { name: headName, value: getHeadMembers(headCount)},
-    { name: 'Positive', value: rolesWithMemberString || 'None', inline: true},
-    { name: 'Negative', value: rolesWithoutMemberString || 'None', inline: true},
+      { name: 'User count', value: list.count.toString()},
+      { name: headName, value: list.head},
+      { name: 'Positive', value: list.positive, inline: true},
+      { name: 'Negative', value: list.negative, inline: true},
     )
     .setTimestamp();
 
-  interaction.reply({ embeds: [embedToSend]});
+  await interaction.editReply({ embeds: [embedToSend] });
 };
